91df4a8134a2df716087f247cb7ea667aea3915b
pc2-PC\pc2 <cesarmencia7@gmail.com>
2018-11-08 10:09:57 -0600

    no message

---
+++ WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        horas.Text = DateAndTime.Now.ToLongTimeString()
+        calendario.Text = DateAndTime.Now.ToLongDateString()
+    End Sub
+
+
+End Class
