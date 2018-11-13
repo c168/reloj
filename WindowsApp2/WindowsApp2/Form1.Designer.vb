18df6bed28ec0bba537341e98cfb3d7c5a8e74cb
pc2-PC\pc2 <cesarmencia7@gmail.com>
2018-11-13 10:48:27 -0600

    no message

---
+++ WindowsApp2/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.horas = New System.Windows.Forms.Label()
+        Me.calendario = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'horas
+        '
+        Me.horas.AutoSize = True
+        Me.horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 60.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.horas.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.horas.Location = New System.Drawing.Point(203, 53)
+        Me.horas.Name = "horas"
+        Me.horas.Size = New System.Drawing.Size(276, 91)
+        Me.horas.TabIndex = 0
+        Me.horas.Text = "Label1"
+        '
+        'calendario
+        '
+        Me.calendario.AutoSize = True
+        Me.calendario.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.calendario.ForeColor = System.Drawing.SystemColors.ScrollBar
+        Me.calendario.Location = New System.Drawing.Point(43, 165)
+        Me.calendario.Name = "calendario"
+        Me.calendario.Size = New System.Drawing.Size(139, 46)
+        Me.calendario.TabIndex = 1
+        Me.calendario.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(0, 3)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(10, 20)
+        Me.DateTimePicker1.TabIndex = 2
+        Me.DateTimePicker1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(687, 261)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.calendario)
+        Me.Controls.Add(Me.horas)
+        Me.Name = "Form1"
+        Me.Text = "cesar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents horas As Label
+    Friend WithEvents calendario As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
